11cec1d107ba49e58c03242ac16cd61839349169
idkngrid <ingrid.camargopr@gmail.com>
2022-06-27 16:58:42 -0300

    add codegen config

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import  ptBR from 'date-fns/locale/pt-BR'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps {
     title: string;
@@ -11,21 +12,29 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps){
+    const { slug } = useParams<{ slug: string }>()
+
     const isLessonAvailable = isPast(props.availableAt);
     const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
         locale: ptBR
-    });
+    })
+
+    const isActiveLesson = slug === props.slug;
 
     return (
         <Link to={`/event/lesson/${props.slug}`} className="group"> 
             <span className=" block text-gray-300 mb-2">
                 {availableDateFormatted}
-            </span> 
+            </span>
 
-            <div className="border border-gray-600 rounded p-4 group-hover:border-green-500">
+            <div className={classNames('border border-gray-600 rounded p-4 group-hover:border-green-500', {
+                'bg-green-500': isActiveLesson,
+            })}>
                 <header className="flex justify-between items-center mb-4">
                     {isLessonAvailable ? (
-                        <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                        <span className={classNames('flex items-center gap-2 text-sm text-blue-500 font-medium', {
+                            'text-white': isActiveLesson
+                        })}>
                         <CheckCircle size={20} />
                         Conteúdo liberado
                         </span>
@@ -35,12 +44,16 @@ export function Lesson(props: LessonProps){
                         Em breve
                         </span>
                     )}
-                    <span className="text-xs font-bold py-[0.125rem] px-2 border border-green-300 rounded">
+                    <span className={classNames('text-xs font-bold py-[0.125rem] px-2 border border-green-300 rounded', {
+                        'border-white': isActiveLesson
+                    })}>
                         {props.type === "live" ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
 
-                <strong className="text-gray-200">{props.title}</strong>
+                <strong className={classNames('text-gray-200', {
+                    'text-white': isActiveLesson
+                })}>{props.title}</strong>
             </div>
         </Link>
     )
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,19 +1,35 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { DiscordLogo, Lightning, FileArrowDown, CaretRight, ImageSquare } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
     lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
+    const { data } = useGetLessonBySlugQuery({
+        variables: {
+            slug: props.lessonSlug,
+        }
+    })
+
+    if (!data || !data.lesson) {
+        return (
+            <div className="flex-1">
+                <p>Carregando...</p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId="SO4-izct7Mc" />
+                        <Youtube videoId={data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -23,24 +39,26 @@ export function Video(props: VideoProps) {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold mb-4">
-                            Aula 01 - Criando o projeto e realizando o setup inicial
+                            {data.lesson.title}
                         </h1>
                         <p className="text-gray-200 leading-relaxed mb-6">
-                            Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
+                            {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mb-20">
-                            <img    
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/diego3g.png" 
-                                alt="" 
-                            />
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mb-20">
+                                <img    
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt="" 
+                                />
 
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">Diego Fernandes</strong>
-                                <span className="text-sm text-gray-200 block">Co-fundador e CTO na Rocketseat</span>
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                    <span className="text-sm text-gray-200 block">{data.lesson.teacher.bio}</span>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                     <div className="flex flex-col gap-4 text-center">
                         <a href="#" className="bg-green-500 flex items-center justify-center gap-2 text-sm font-bold uppercase p-4 rounded hover:bg-green-700 transition-colors">
